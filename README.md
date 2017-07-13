b36f3d4a9e5e24caa2cf22bee8837e737ec8b54c
Joris Ooms <info@jorisooms.be>
2017-07-13 19:31:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Slack Crypto bot
+
+Fetches cryptocurrency ticker information from CoinMarketCap.
+
+## Running
+
+- Create a bot user and grab the API token
+- Clone this repo and run `token=<your_token> node index.js`
+
+## Usage:
+
+- Invite the bot in your channels
+- `@cryptobot eth` will display ethereum information
+- `@cryptobot btc` will display bitcoin information
---
+++ index.js
@@ -0,0 +1,78 @@
+const Botkit = require('botkit');
+const axios = require('axios');
+const os = require('os');
+const { chain } = require('ramda');
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+const BOT_TRIGGERS = ['direct_message', 'direct_mention'];
+const API_ENDPOINT = 'https://api.coinmarketcap.com/v1/ticker';
+
+const ETH_ENDPOINT_EUR = `${API_ENDPOINT}/ethereum?convert=EUR`;
+const BTC_ENDPOINT_EUR = `${API_ENDPOINT}/bitcoin?convert=EUR`;
+
+const controller = Botkit.slackbot({
+  debug: false,
+});
+
+const bot = controller
+  .spawn({
+    token: process.env.token,
+  })
+  .startRTM();
+
+const fetchApiResponse = url =>
+  axios
+    .get(url)
+    .then(response => response.data)
+    .then(data =>
+      chain(
+        currency => [
+          {
+            title: 'Price (EUR)',
+            value: `€${currency.price_eur}`,
+            short: true,
+          },
+          {
+            title: 'Price (USD)',
+            value: `$${currency.price_usd}`,
+            short: true,
+          },
+          (() =>
+            currency.percent_change_1h && {
+              title: '% Change (1h)',
+              value: `${currency.percent_change_1h}%`,
+              short: true,
+            })(),
+          {
+            title: '% Change (24h)',
+            value: `${currency.percent_change_24h}%`,
+            short: true,
+          },
+        ],
+        data
+      )
+    )
+    .then(data => [{ fields: data }])
+    .catch(console.log);
+
+controller.hears(['eth', 'ethereum'], BOT_TRIGGERS, (bot, message) =>
+  fetchApiResponse(ETH_ENDPOINT_EUR).then(data =>
+    bot.reply(message, {
+      text: '*Ethereum (ETH)*',
+      attachments: data,
+    })
+  )
+);
+
+controller.hears(['bitcoin', 'btc'], BOT_TRIGGERS, (bot, message) =>
+  fetchApiResponse(BTC_ENDPOINT_EUR).then(data =>
+    bot.reply(message, {
+      text: '*Bitcoin (BTC)*',
+      attachments: data,
+    })
+  )
+);
