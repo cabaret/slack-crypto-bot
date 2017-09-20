fb4c159880688d8a042e95ce1cc9ea8d1e2059ec
Joris Ooms <hello@cupofcode.be>
2017-09-20 22:04:35 +0200

    All the coins

--- index.js
+++ index.js
@@ -1,88 +1,132 @@
-const Botkit = require('botkit');
-const axios = require('axios');
-const os = require('os');
-const { chain } = require('ramda');
+import Botkit from 'botkit';
+import axios from 'axios';
+import {
+  compose,
+  contains,
+  curry,
+  flip,
+  head,
+  ifElse,
+  keys,
+  prop,
+  reduce,
+} from 'ramda';
 
 if (!process.env.token) {
-  console.log('Error: Specify token in environment');
+  console.log('Error: Specify token in environment'); // eslint-disable-line
   process.exit(1);
 }
 
-const BOT_TRIGGERS = ['direct_message', 'direct_mention'];
+const RESPOND_TO = ['direct_message', 'direct_mention'];
 const API_ENDPOINT = 'https://api.coinmarketcap.com/v1/ticker';
 
-const ETH_ENDPOINT_EUR = `${API_ENDPOINT}/ethereum?convert=EUR`;
-const BTC_ENDPOINT_EUR = `${API_ENDPOINT}/bitcoin?convert=EUR`;
-const LTC_ENDPOINT_EUR = `${API_ENDPOINT}/litecoin?convert=EUR`;
+const getCoinEndpoint = coin => `${API_ENDPOINT}/${coin}?convert=EUR`;
 
-const controller = Botkit.slackbot({
-  debug: false,
+const createController = () =>
+  Botkit.slackbot({
+    debug: false,
+  });
+
+const fetch = async url => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const normalize = reduce((acc, coin) => ({ ...acc, [coin.id]: coin }), {});
+
+const getBotTriggers = reduce(
+  (acc, coin) => ({
+    ...acc,
+    [coin.id]: coin.id,
+    [coin.symbol.toLowerCase()]: coin.id,
+  }),
+  {},
+);
+
+const isValidCoin = curry((coins, coin) =>
+  compose(contains(coin), keys)(coins),
+);
+
+const getId = flip(prop);
+
+const buildAttachments = coin => [
+  {
+    fields: [
+      {
+        title: 'Price (EUR)',
+        value: `€${coin.price_eur}`,
+        short: true,
+      },
+
+      {
+        title: 'Price (USD)',
+        value: `$${coin.price_usd}`,
+        short: true,
+      },
+
+      coin.percent_change_1h && {
+        title: '% Change (1h)',
+        value: `${coin.percent_change_1h}%`,
+        short: true,
+      },
+
+      {
+        title: '% Change (24h)',
+        value: `${coin.percent_change_24h}%`,
+        short: true,
+      },
+    ],
+  },
+];
+
+const formatText = coin => `*${coin.name} (${coin.symbol})*`;
+
+const sendValidReply = curry(async (bot, message, id) => {
+  const coinData = await fetch(getCoinEndpoint(id));
+  const coin = head(coinData);
+  const attachments = buildAttachments(coin);
+
+  bot.reply(message, {
+    text: formatText(coin),
+    attachments,
+  });
 });
 
-const bot = controller
-  .spawn({
-    token: process.env.token,
-  })
-  .startRTM();
+const sendErrorReply = curry((bot, message, text) => {
+  bot.reply(message, {
+    text: `${text} is not a valid coin. Check coinmarketcap.com.`,
+  });
+});
 
-const fetchApiResponse = url =>
-  axios
-    .get(url)
-    .then(response => response.data)
-    .then(data =>
-      chain(
-        currency => [
-          {
-            title: 'Price (EUR)',
-            value: `€${currency.price_eur}`,
-            short: true,
-          },
-          {
-            title: 'Price (USD)',
-            value: `$${currency.price_usd}`,
-            short: true,
-          },
-          (() =>
-            currency.percent_change_1h && {
-              title: '% Change (1h)',
-              value: `${currency.percent_change_1h}%`,
-              short: true,
-            })(),
-          {
-            title: '% Change (24h)',
-            value: `${currency.percent_change_24h}%`,
-            short: true,
-          },
-        ],
-        data
-      )
-    )
-    .then(data => [{ fields: data }])
-    .catch(console.log);
+const getReply = async (botTriggers, coins, bot, message) =>
+  ifElse(
+    isValidCoin(botTriggers),
+    compose(sendValidReply(bot, message), getId(botTriggers)),
+    sendErrorReply(bot, message),
+  )(message.text);
 
-controller.hears(['eth', 'ethereum'], BOT_TRIGGERS, (bot, message) =>
-  fetchApiResponse(ETH_ENDPOINT_EUR).then(attachments =>
-    bot.reply(message, {
-      text: '*Ethereum (ETH)*',
-      attachments,
+const startBot = async () => {
+  const controller = createController();
+
+  controller
+    .spawn({
+      token: process.env.token,
     })
-  )
-);
+    .startRTM();
 
-controller.hears(['bitcoin', 'btc'], BOT_TRIGGERS, (bot, message) =>
-  fetchApiResponse(BTC_ENDPOINT_EUR).then(attachments =>
-    bot.reply(message, {
-      text: '*Bitcoin (BTC)*',
-      attachments,
-    })
-  )
-);
+  const coinData = await fetch(API_ENDPOINT);
+  const coins = normalize(coinData);
+  const botTriggers = getBotTriggers(coinData);
 
-controller.hears(['litecoin', 'ltc'], BOT_TRIGGERS, (bot, message) =>
-  fetchApiResponse(LTC_ENDPOINT_EUR).then(attachments =>
-    bot.reply(message, {
-      text: '*Litecoin (LTC)*',
-      attachments,
-    })
-  )
-);
+  controller.hears(
+    keys(botTriggers),
+    RESPOND_TO,
+    curry(getReply)(botTriggers, coins),
+  );
+};
+
+startBot();
